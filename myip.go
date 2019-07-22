71e1afb788d117d0db301cf5f62734ae86f1aaa1
spitfire88 <sanrio.alvares@intel.com>
2019-07-22 16:05:51 -0700

    Get my ip from Go
    Get IP from container running this application

---
+++ myip.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+URL: https://github.com/mccoyst/myip/blob/master/myip.go
+URL: http://changsijay.com/2013/07/28/golang-get-ip-address/
+*/
+
+import (
+	"net"
+	"os"
+)
+
+func main() {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+			}
+		}
+	}
+}
